de64b8b512766c88c4d32bef858ee6a24e76239c
dianziyoujian@mail.com <dianziyoujian@mail.com>
2017-11-22 18:50:36 +0800

    first commit

---
+++ renewRoutes.py
@@ -0,0 +1,26 @@
+#to renew the route of openvpn 
+import os,glob
+routefile="routes.txt"
+if os.path.exists(routefile):
+    os.remove(routefile)
+##since chnroutes is too large, i choose the bestroutetb which is a node based tools from https://github.com/ashi009/bestroutetb
+##just put this file into the location of ovpn config files    
+os.popen("bestroutetb --route.vpn=us -p openvpn -o "+routefile)
+
+f=open(routefile)
+route=f.read()
+f.close()
+files=glob.glob("*.ovpn")
+for fname in files :
+    f=open(fname, 'r') 
+    print fname
+    content=[]
+    for l in f:
+        if l.strip().split(" ")[0]!="route":
+            content.append(l);
+    f.close()
+    open(fname,"w").write("".join(content))
+    open(fname,"a").write("\r\n"+route)
+os.remove(routefile) 
+    
+
